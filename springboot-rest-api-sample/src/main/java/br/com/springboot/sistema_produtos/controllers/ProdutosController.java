817c8b80b513330687a45f366357edc308b7202e
gilvan alves <gilvan.office@gmail.com>
2023-08-31 12:35:35 -0300

    criação do endpoint salvar

--- springboot-rest-api-sample/src/main/java/br/com/springboot/sistema_produtos/controllers/ProdutosController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/sistema_produtos/controllers/ProdutosController.java
@@ -1,23 +1,42 @@
 package br.com.springboot.sistema_produtos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * A sample greetings controller to return greeting text
- */
+import br.com.springboot.sistema_produtos.models.Produto;
+import br.com.springboot.sistema_produtos.repository.ProdutoRepository;
+
+
 @RestController
 public class ProdutosController {
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@PostMapping(value="salvar")
+	@ResponseBody
+	public ResponseEntity<Produto> salvar(@RequestBody Produto produto){
+		Produto product = produtoRepository.save(produto);
+		return new ResponseEntity<Produto>(product, HttpStatus.CREATED);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+   
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String name) {
