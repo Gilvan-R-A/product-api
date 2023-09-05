aeb162bc96f0848c85a29bb653609376c84f6180
gilvan alves <gilvan.office@gmail.com>
2023-09-05 14:19:58 -0300

    criação do endpoint deletar

--- springboot-rest-api-sample/src/main/java/br/com/springboot/sistema_produtos/controllers/ProdutosController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/sistema_produtos/controllers/ProdutosController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,8 +49,14 @@ public class ProdutosController {
 			return new ResponseEntity<String>("Id não foi informado para atualização.", HttpStatus.OK);	
 		}
 		Produto product = produtoRepository.saveAndFlush(produto);
-		return new ResponseEntity<Produto>(product, HttpStatus.OK);
-		
+		return new ResponseEntity<Produto>(product, HttpStatus.OK);	
+	}
+	
+	@DeleteMapping(value = "delete")
+	@ResponseBody
+	public ResponseEntity<String>delete(@RequestParam Long idProduto){
+		produtoRepository.deleteById(idProduto); 
+		return new ResponseEntity<String>("Produto deletado com sucesso!", HttpStatus.OK);
 	}
 	
 	
