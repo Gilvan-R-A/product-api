0efdacf3b09f98cea7b6d0098d387b6c4b93bc2f
gilvan alves <gilvan.office@gmail.com>
2023-09-05 15:40:58 -0300

    criação do endpoint buscarPorNome

--- springboot-rest-api-sample/src/main/java/br/com/springboot/sistema_produtos/controllers/ProdutosController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/sistema_produtos/controllers/ProdutosController.java
@@ -37,14 +37,14 @@ public class ProdutosController {
 	
 	@GetMapping(value = "listatodos")
 	@ResponseBody
-	public ResponseEntity<List<Produto>>listaProduto(){
+	public ResponseEntity<List<Produto>> listaProduto(){
 		List<Produto> produtos = produtoRepository.findAll();
 		return new ResponseEntity<List<Produto>>(produtos, HttpStatus.OK);
 	}
 	
 	@PutMapping(value = "atualizar")
 	@ResponseBody
-	public ResponseEntity<?>atualizar(@RequestBody Produto produto){
+	public ResponseEntity<?> atualizar(@RequestBody Produto produto){
 		if(produto.getId() == null) {
 			return new ResponseEntity<String>("Id não foi informado para atualização.", HttpStatus.OK);	
 		}
@@ -52,20 +52,25 @@ public class ProdutosController {
 		return new ResponseEntity<Produto>(product, HttpStatus.OK);	
 	}
 	
-	
 	@DeleteMapping(value = "delete")
 	@ResponseBody
-	public ResponseEntity<String>delete(@RequestParam Long idProduto){
+	public ResponseEntity<String> delete(@RequestParam Long idProduto){
 		produtoRepository.deleteById(idProduto); 
 		return new ResponseEntity<String>("Produto deletado com sucesso!", HttpStatus.OK);
 	}
-	
 	
 	@GetMapping(value = "buscarProdutoPorId")
 	@ResponseBody
 	public ResponseEntity<Produto> buscaProdutoPorId(@RequestParam(name = "idProduto") Long idProduto){
 		Produto produto = produtoRepository.findById(idProduto).get();
 		return new ResponseEntity<Produto>(produto, HttpStatus.OK);
+	}
+	 
+	@GetMapping(value = "buscarPorNome")
+	@ResponseBody
+	public ResponseEntity<List<Produto>> buscarPorNome(@RequestParam(name = "name") String name){
+		List<Produto> produto = produtoRepository.buscarPorNome(name.trim().toUpperCase());
+		return new ResponseEntity<List<Produto>>(produto, HttpStatus.OK);
 	}
 	
 	
--- springboot-rest-api-sample/src/main/java/br/com/springboot/sistema_produtos/repository/ProdutoRepository.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/sistema_produtos/repository/ProdutoRepository.java
@@ -1,11 +1,17 @@
 package br.com.springboot.sistema_produtos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.springboot.sistema_produtos.models.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	@Query(value = "select p from Produto p where upper(trim(p.nome)) like %?1%")
+	List<Produto> buscarPorNome(String name);
 
 }
